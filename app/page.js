f0f6879d64b407893f903f80be090e2db58c889b
Kshitij Kochhar <kshitij@ctfguide.com>
2024-05-06 23:58:28 -0400

    Updated favicon + head

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
-
 import Image from "next/image";
 import Hero from "./components/Hero";
+import Head from "next/head";
 
 export default function Home() {
-  return (
-    <main className="">
-      <Hero />
+    return (
+        <>
+            <main className="">
+                <Hero/>
 
-    </main>
-  );
+            </main>
+        </>
+    );
 }
